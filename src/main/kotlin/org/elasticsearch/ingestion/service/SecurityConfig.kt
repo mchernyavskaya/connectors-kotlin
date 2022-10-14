3a5fe2e45d665dc916c7b156b8d69bf202cd1ecb
Maryna Cherniavska <maryna.cherniavska@elastic.co>
2022-10-14 16:29:03 +0200

    trying to configure oauth provider for web security

--- src/main/kotlin/org/elasticsearch/ingestion/service/SecurityConfig.kt
+++ src/main/kotlin/org/elasticsearch/ingestion/service/SecurityConfig.kt
@@ -3,9 +3,10 @@ package org.elasticsearch.ingestion.service
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
-import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
@@ -27,8 +28,17 @@ class SecurityConfig {
                     it.anyRequest().authenticated()
                 }
             )
-            .oauth2Client(withDefaults())
+            .oauth2Login()
+            .clientRegistrationRepository(clientRegistrationRepository())
+            .authorizedClientService(authorizedClientService())
         return http.build()
+    }
+
+    @Bean
+    fun authorizedClientService(): OAuth2AuthorizedClientService? {
+        return InMemoryOAuth2AuthorizedClientService(
+            clientRegistrationRepository()
+        )
     }
 
     @Bean
