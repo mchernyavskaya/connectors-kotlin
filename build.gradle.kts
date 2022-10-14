9f30397bf56c1e2c72659dd32003873adbf49d2f
Maryna Cherniavska <maryna.cherniavska@elastic.co>
2022-10-14 16:23:30 +0200

    trying to configure oauth provider for web security

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ extra["testcontainersVersion"] = "1.17.4"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
---
+++ src/main/kotlin/org/elasticsearch/ingestion/service/SecurityConfig.kt
@@ -0,0 +1,53 @@
+package org.elasticsearch.ingestion.service
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.web.SecurityFilterChain
+import java.util.*
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeRequests(
+                Customizer {
+                    it.anyRequest().authenticated()
+                }
+            )
+            .oauth2Client(withDefaults())
+        return http.build()
+    }
+
+    @Bean
+    fun clientRegistrationRepository(): ClientRegistrationRepository? {
+        val registrations = listOf<ClientRegistration>(
+            ClientRegistration.withRegistrationId("wordpress")
+                .clientId("client-id")
+                .clientSecret("client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .authorizationUri("https://public-api.wordpress.com/oauth2/authorize")
+                .tokenUri("https://public-api.wordpress.com/oauth2/token")
+                .userInfoUri("https://public-api.wordpress.com/rest/v1/me")
+                .userNameAttributeName("username")
+                .jwkSetUri("https://public-api.wordpress.com/oauth2/jwks")
+                .clientName("WordPress")
+                .build()
+        )
+        return InMemoryClientRegistrationRepository(registrations)
+    }
+}
